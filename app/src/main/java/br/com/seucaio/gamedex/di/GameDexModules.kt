13eb10565a7cc6e86b25a449ef08dc5e0f1c4f6b
seucaio <caiopimentel8@gmail.com>
2025-11-13 20:34:48 -0300

    feat: Add domain module and use cases to Koin
    Integrates the `domain` module into the `app` module and sets up the dependency injection for the domain-layer use cases using Koin.
    
    Key changes:
    - Adds a project dependency on the `:domain` module in the `app/build.gradle.kts`.
    - Creates a `domainModule` in `GameDexModules.kt` to provide use cases.
    - Injects `GetGenresUseCase`, `GetGenreDetailByIdUseCase`, `GetPlatformsUseCase`, and `GetPlatformDetailByIdUseCase`.
    - Adds the new `domainModule` to the list of Koin modules.

--- app/src/main/java/br/com/seucaio/gamedex/di/GameDexModules.kt
+++ app/src/main/java/br/com/seucaio/gamedex/di/GameDexModules.kt
@@ -1,5 +1,20 @@
 package br.com.seucaio.gamedex.di
 
-import org.koin.core.module.Module
+import br.com.seucaio.gamedex.repository.GenresRepository
+import br.com.seucaio.gamedex.repository.PlatformsRepository
+import br.com.seucaio.gamedex.usecase.genre.GetGenreDetailByIdUseCase
+import br.com.seucaio.gamedex.usecase.genre.GetGenresUseCase
+import br.com.seucaio.gamedex.usecase.platform.GetPlatformDetailByIdUseCase
+import br.com.seucaio.gamedex.usecase.platform.GetPlatformsUseCase
+import org.koin.dsl.module
 
-val gameDexModules = emptyList<Module>()
+val domainModule = module {
+    factory { GetGenresUseCase(repository = get<GenresRepository>()) }
+    factory { GetGenreDetailByIdUseCase(repository = get<GenresRepository>()) }
+    factory { GetPlatformsUseCase(repository = get<PlatformsRepository>()) }
+    factory { GetPlatformDetailByIdUseCase(repository = get<PlatformsRepository>()) }
+}
+
+val gameDexModules = listOf(
+    domainModule
+)
