a4ead771c7d84052aae222e5e88324516ebfc3a1
seucaio <caiopimentel8@gmail.com>
2025-11-12 21:02:31 -0300

    feat: Integrate Koin for dependency injection
    Adds the Koin dependency injection framework to the project.
    
    Key changes:
    - Adds `koin-android` and `koin-androidx-compose` dependencies.
    - Creates a `GameDexApplication` class and configures Koin within it.
    - Registers the `GameDexApplication` in the `AndroidManifest.xml`.
    - Sets up an initial empty module list in `GameDexModules.kt`.

---
+++ app/src/main/java/br/com/seucaio/gamedex/GameDexApplication.kt
@@ -0,0 +1,23 @@
+package br.com.seucaio.gamedex
+
+import android.app.Application
+import br.com.seucaio.gamedex.di.gameDexModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+
+class GameDexApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        configureKoin()
+    }
+
+    private fun configureKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@GameDexApplication)
+            modules(gameDexModules)
+        }
+    }
+}
---
+++ app/src/main/java/br/com/seucaio/gamedex/di/GameDexModules.kt
@@ -0,0 +1,5 @@
+package br.com.seucaio.gamedex.di
+
+import org.koin.core.module.Module
+
+val gameDexModules = emptyList<Module>()
