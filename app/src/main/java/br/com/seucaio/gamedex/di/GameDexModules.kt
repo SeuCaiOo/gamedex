8069a4487831424a9c5cc5cb77af2744087c5dfd
seucaio <caiopimentel8@gmail.com>
2025-11-15 18:40:58 -0300

    feat: Implement navigation and dependency injection for screens
    Adds the core navigation graph and dependency injection setup for the application's screens, enabling screen transitions and providing ViewModels with necessary use cases.
    
    Key changes:
    - **`GameDexNavGraph.kt`**: Implements composable destinations for `HomeScreen`, `PlatformListScreen`, and `PlatformDetailsScreen`, defining the navigation logic between them.
    - **`GameDexModules.kt`**: Creates a new `appModule` to provide `PlatformListViewModel` and `PlatformDetailsViewModel` using Koin.
    - **`AndroidManifest.xml`**: Adds the `INTERNET` permission to allow the application to make network requests for fetching data.

--- app/src/main/java/br/com/seucaio/gamedex/di/GameDexModules.kt
+++ app/src/main/java/br/com/seucaio/gamedex/di/GameDexModules.kt
@@ -1,10 +1,30 @@
 package br.com.seucaio.gamedex.di
 
+import br.com.seucaio.gamedex.ui.features.platforms.detail.viewmodel.PlatformDetailsViewModel
+import br.com.seucaio.gamedex.ui.features.platforms.list.viewmodel.PlatformListViewModel
+import br.com.seucaio.gamedex.usecase.platform.GetPlatformDetailByIdUseCase
+import br.com.seucaio.gamedex.usecase.platform.GetPlatformsUseCase
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
 val domainModule = provideDomainModule()
 
 val dataModule = provideDataModule()
 
+val appModule = module {
+    viewModel {
+        PlatformListViewModel(getPlatformsUseCase = get<GetPlatformsUseCase>())
+    }
+    viewModel { (id: Int) ->
+        PlatformDetailsViewModel(
+            id = id,
+            getPlatformByIdUseCase = get<GetPlatformDetailByIdUseCase>(),
+        )
+    }
+}
+
 val gameDexModules = listOf(
     domainModule,
-    dataModule
+    dataModule,
+    appModule
 )
