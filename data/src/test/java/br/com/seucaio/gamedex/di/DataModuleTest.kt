c21b52e1a3512c274929a01404a86fc01fc89d44
seucaio <caiopimentel8@gmail.com>
2025-11-15 02:42:33 -0300

    test: Add Koin module verification for data layer
    Adds a Koin module test to verify the dependency graph for the `data` module. This test uses `checkModules` to ensure that all dependencies provided in `provideDataModule` can be resolved correctly.
    
    An Android `Context` is mocked to satisfy the requirements of the database module during the test.

---
+++ data/src/test/java/br/com/seucaio/gamedex/di/DataModuleTest.kt
@@ -0,0 +1,19 @@
+package br.com.seucaio.gamedex.di
+
+import android.content.Context
+import io.mockk.mockk
+import org.junit.Test
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+import org.koin.test.check.checkModules
+
+class DataModuleTest : KoinTest {
+
+    @Test
+    fun `check all modules from data`() {
+        checkModules {
+            // Mock Android Context dependency, which is required by the database module
+            modules(provideDataModule(), module { single { mockk<Context>(relaxed = true) } })
+        }
+    }
+}
