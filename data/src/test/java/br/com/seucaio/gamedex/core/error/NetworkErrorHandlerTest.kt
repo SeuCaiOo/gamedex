c3a283fc30036a4e7c0c1f5bae4927feb950afa8
seucaio <caiopimentel8@gmail.com>
2025-11-15 13:32:30 -0300

    refactor: Rename `handleError` to `handleNetworkError`
    Renames the `handleError` extension function to `handleNetworkError` to more accurately reflect its purpose of handling network-specific exceptions.
    
    This change clarifies the function's scope and improves code readability. All corresponding unit tests in `NetworkErrorHandlerTest` have been updated to use the new function name.

--- data/src/test/java/br/com/seucaio/gamedex/core/error/NetworkErrorHandlerTest.kt
+++ data/src/test/java/br/com/seucaio/gamedex/core/error/NetworkErrorHandlerTest.kt
@@ -1,6 +1,6 @@
 package br.com.seucaio.gamedex.core.error
 
-import br.com.seucaio.gamedex.core.error.NetworkErrorHandler.handleError
+import br.com.seucaio.gamedex.core.error.NetworkErrorHandler.handleNetworkError
 import br.com.seucaio.gamedex.util.exception.DomainException
 import kotlinx.serialization.SerializationException
 import okhttp3.MediaType.Companion.toMediaType
@@ -17,67 +17,67 @@ import java.net.UnknownHostException
 class NetworkErrorHandlerTest {
 
     @Test
-    fun `handleError should return NetworkUnavailableException when network is unavailable`() {
+    fun `handleNetworkError should return NetworkUnavailableException when network is unavailable`() {
         // Given
         val error = RuntimeException()
 
         // When
-        val result = error.handleError(isNetworkAvailable = false)
+        val result = error.handleNetworkError(isNetworkAvailable = false)
 
         // Then
         assertTrue(result is DomainException.NetworkUnavailableException)
     }
 
     @Test
-    fun `handleError should return NoInternetException for UnknownHostException`() {
+    fun `handleNetworkError should return NoInternetException for UnknownHostException`() {
         // Given
         val error = Exception(UnknownHostException())
 
         // When
-        val result = error.handleError(isNetworkAvailable = true)
+        val result = error.handleNetworkError(isNetworkAvailable = true)
 
         // Then
         assertTrue(result is DomainException.NoInternetException)
     }
 
     @Test
-    fun `handleError should return NoInternetException for SocketException`() {
+    fun `handleNetworkError should return NoInternetException for SocketException`() {
         // Given
         val error = Exception(SocketException())
 
         // When
-        val result = error.handleError(isNetworkAvailable = true)
+        val result = error.handleNetworkError(isNetworkAvailable = true)
 
         // Then
         assertTrue(result is DomainException.NoInternetException)
     }
 
     @Test
-    fun `handleError should return TimeoutException for SocketTimeoutException`() {
+    fun `handleNetworkError should return TimeoutException for SocketTimeoutException`() {
         // Given
         val error = Exception(SocketTimeoutException())
 
         // When
-        val result = error.handleError(isNetworkAvailable = true)
+        val result = error.handleNetworkError(isNetworkAvailable = true)
 
         // Then
         assertTrue(result is DomainException.TimeoutException)
     }
 
     @Test
-    fun `handleError should return TimeoutException for InterruptedIOException`() {
+    fun `handleNetworkError should return TimeoutException for InterruptedIOException`() {
         // Given
         val error = Exception(InterruptedIOException())
 
         // When
-        val result = error.handleError(isNetworkAvailable = true)
+        val result = error.handleNetworkError(isNetworkAvailable = true)
 
         // Then
         assertTrue(result is DomainException.TimeoutException)
     }
 
     @Test
-    fun `handleError should return ServerException for HttpException with code 500`() {
+    fun `handleNetworkError should return ServerException for HttpException with code 500`() {
         // Given
         val httpException = HttpException(
             Response.error<Any>(500, "".toResponseBody("application/json".toMediaType()))
@@ -85,14 +85,14 @@ class NetworkErrorHandlerTest {
         val error = Exception(httpException)
 
         // When
-        val result = error.handleError(isNetworkAvailable = true)
+        val result = error.handleNetworkError(isNetworkAvailable = true)
 
         // Then
         assertTrue(result is DomainException.ServerException)
     }
 
     @Test
-    fun `handleError should return ApiException for HttpException with code 404`() {
+    fun `handleNetworkError should return ApiException for HttpException with code 404`() {
         // Given
         val httpException = HttpException(
             Response.error<Any>(404, "".toResponseBody("application/json".toMediaType()))
@@ -100,43 +100,43 @@ class NetworkErrorHandlerTest {
         val error = Exception(httpException)
 
         // When
-        val result = error.handleError(isNetworkAvailable = true)
+        val result = error.handleNetworkError(isNetworkAvailable = true)
 
         // Then
         assertTrue(result is DomainException.ApiException)
     }
 
     @Test
-    fun `handleError should return ParseException for SerializationException`() {
+    fun `handleNetworkError should return ParseException for SerializationException`() {
         // Given
         val error = Exception(SerializationException())
 
         // When
-        val result = error.handleError(isNetworkAvailable = true)
+        val result = error.handleNetworkError(isNetworkAvailable = true)
 
         // Then
         assertTrue(result is DomainException.ParseException)
     }
 
     @Test
-    fun `handleError should return UnknownException for other exceptions`() {
+    fun `handleNetworkError should return UnknownException for other exceptions`() {
         // Given
         val error = Exception(IllegalArgumentException())
 
         // When
-        val result = error.handleError(isNetworkAvailable = true)
+        val result = error.handleNetworkError(isNetworkAvailable = true)
 
         // Then
         assertTrue(result is DomainException.UnknownException)
     }
 
     @Test
-    fun `handleError should return UnknownException for null cause`() {
+    fun `handleNetworkError should return UnknownException for null cause`() {
         // Given
         val error = Exception() // Sem causa
 
         // When
-        val result = error.handleError(isNetworkAvailable = true)
+        val result = error.handleNetworkError(isNetworkAvailable = true)
 
         // Then
         assertTrue(result is DomainException.UnknownException)
