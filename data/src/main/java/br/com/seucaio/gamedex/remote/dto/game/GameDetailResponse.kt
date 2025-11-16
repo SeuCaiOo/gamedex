f45175503cba6c2980ab5b835e4c87405e9a2072
seucaio <caiopimentel8@gmail.com>
2025-11-16 03:51:23 -0300

    feat: Add DTOs for game data responses
    Introduces new Data Transfer Objects (DTOs) to model the responses from the game data API, specifically for game details, game list items, and screenshots. These classes are designed for deserialization using `kotlinx.serialization`.
    
    **Key changes:**
    - **`GameDetailResponse.kt`**: Defines a comprehensive data model for a single game's detailed information, including properties like images, description, ratings, platforms, stores, developers, and more. It also includes nested classes for platform and store-specific details.
    - **`GameItemResponse.kt`**: Creates a data class representing a game item as it appears in a list. This is a more concise model containing essential information like ID, name, background image, and ratings.
    - **`GameScreenshotResponse.kt`**: Adds a simple data class to model a game screenshot, containing an ID and an image URL.

---
+++ data/src/main/java/br/com/seucaio/gamedex/remote/dto/game/GameDetailResponse.kt
@@ -0,0 +1,61 @@
+package br.com.seucaio.gamedex.remote.dto.game
+
+import br.com.seucaio.gamedex.remote.dto.list.GameDataListInfoGamesResponse
+import br.com.seucaio.gamedex.remote.dto.list.GameDataListInfoResponse
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GameDetailResponse(
+    val id: Int,
+    val name: String,
+    @SerialName("background_image")
+    val imageBackground: String? = null,
+    @SerialName("background_image_additional")
+    val imageBackgroundAdditional: String? = null,
+    val description: String,
+    val metacritic: Int? = null,
+    val released: String? = null,
+    val website: String? = null,
+    val rating: Double? = null,
+    @SerialName("alternative_names")
+    val alternativeNames: List<String>? = null,
+    val platforms: List<PlatformInfoDetailResponse<GameDataListInfoResponse>>? = null,
+    val stores: List<StoreInfoDetailResponse>? = null,
+    val developers: List<GameDataListInfoResponse>? = null,
+    val publishers: List<GameDataListInfoResponse>? = null,
+    val genres: List<GameDataListInfoResponse>? = null,
+    @SerialName("esrb_rating")
+    val esrbRating: GameDataListInfoGamesResponse? = null,
+) {
+    @Serializable
+    data class PlatformInfoDetailResponse<T>(
+        @SerialName("platform") val info: T,
+        @SerialName("released_at")
+        val releasedAt: String? = null,
+        val requirements: PlatformRequirementsResponse? = null
+    ) {
+        @Serializable
+        data class PlatformRequirementsResponse(
+            val minimum: String? = null,
+            val recommended: String? = null
+        )
+    }
+
+    @Serializable
+    data class StoreInfoDetailResponse(
+        val id: Int,
+        @SerialName("store") val info: StoreItemResponse
+    ) {
+        @Serializable
+        data class StoreItemResponse(
+            val id: Int,
+            val name: String,
+            val domain: String? = null,
+            @SerialName("games_count")
+            val gamesCount: Int? = null,
+            @SerialName("image_background")
+            val imageBackground: String? = null,
+        )
+    }
+}
