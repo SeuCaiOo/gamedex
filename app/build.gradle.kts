a8a0e6cd3b00de976117b2dd8e08d1407adf563b
seucaio <caiopimentel8@gmail.com>
2025-11-17 04:30:01 -0300

    chore: Atualiza a versão do app para 1.1.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "br.com.seucaio.gamedex"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0-alpha.1"
+        versionCode = 2
+        versionName = "1.1.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
